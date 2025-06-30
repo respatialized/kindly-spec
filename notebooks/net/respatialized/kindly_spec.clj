b5ea6bd33ea63e5de68f6323d9dafdda1d7913b7
Andrew Foltz-Morrison <thefomo@gmail.com>
2025-06-30 16:49:17 -0400

    Begin adding mutually recursive specification

--- notebooks/net/respatialized/kindly_spec.clj
+++ notebooks/net/respatialized/kindly_spec.clj
@@ -95,3 +95,83 @@
 
 (comment
   (clerk/serve! {:browse true}))
+;; # Specification 2
+
+;; We will take a compositional approach, starting from the necessary metadata.
+
+(def Kind-Properties
+  (m/schema
+   [:map
+    {:description
+     "The properties required by Kindly, either as a map or in the form of metadata."}
+    [:kind {:description "The kind of the value"} :keyword]
+    [:kindly/hide-code
+     {:description "Whether to hide the source expression in the output"
+      :optional    true} :boolean]
+    [:kindly/options
+     {:description
+      "Additional options for the kind. May be kind-specific or general."
+      :optional true}
+     [:maybe
+      [:map
+       [:hide-value
+        {:optional true :description "Whether to hide the value in the output."}
+        :boolean]
+       [:wrapped-value
+        {:optional true
+         :description
+         "Whether the value has been 'wrapped' in a vector to carry Kindly metadata"}
+        :boolean]]]]]))
+
+(def kindly-properties? (m/validator Kind-Properties))
+
+(defn kindly-metadata?
+  [v]
+  (and (instance? clojure.lang.IObj v) (kindly-properties? (meta v))))
+
+(kindly-metadata? ^{:kind :vector} [1 2 3])
+
+(def Wrapped-Value-Properties
+  (mu/merge
+   Kind-Properties
+   [:map
+    {:description
+     "A value carrying metadata indicating that it has been wrapped to support kindly metadata"}
+    [:kindly/options [:map [:wrapped-value [:= true]]]]]))
+
+(defn wrapped-value?
+  [v]
+  (and (vector? v) (m/validate Wrapped-Value-Properties (meta v))))
+
+(m/schema [:and [:fn wrapped-value?] [:vector {:min 1 :max 1} :any]])
+
+(def Kindly-Value
+  (m/schema
+   [:schema
+    {:registry
+     ;; start from the top and proceed downward
+     {::kindly        [:or [:ref ::meta-value] [:ref ::wrapped-value]
+                       [:ref ::kindly-map] #_[:ref ::fragment]]
+      ::meta-value    [:and [:fn kindly-metadata?] [:ref ::value]]
+      ::wrapped-value [:and [:fn wrapped-value?] [:vector {:min 1 :max 1} :any]]
+      ::kindly-map    (mu/merge Kind-Properties
+                                [:map [:code :string] [:form :any]
+                                 [:value :any #_[:ref ::value]]])
+      #_#_::fragment
+        [:or [:and [:fn kindly-metadata?] [:vector [:ref ::kindly]]]
+         (mu/merge Kind-Properties
+                   [:map [:code :string] [:form :any] [:kind [:= :fragment]]
+                    [:value [:vector [:ref ::kindly]]]])]
+      ::value
+      ;; a value is a kindly or plain Clojure value
+      [:or [:ref ::kindly] :any]}} ::kindly]))
+
+(def example-nested-value
+  ^{:kind :fragment}
+  [^{:kind :hiccup}
+   [:div {:class "example"}
+    "An example Hiccup div containing a nested kindly element"
+    (kind/md "Nested **markdown** text in a Hiccup element")]
+   ^{:kind :code} '(+ 3 4 5 6)
+   {:kind :md :value "_More_ markdown text inside of a Kindly map."}
+   ^{:kind :edn} {:a 1 :b 2 :description "EDN map"}])
